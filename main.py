77ab58d6b9fecc9e2ed2ef705e5275a6419bed81
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-09-17 15:58:46 +0300

    Update main.py
    rework shet

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from random import randint
-loc = 1
-while loc > -1:
-    if loc == 1:
-        inNum = int(input("""Ты просыпаешься.
+
+inv = dict() 
+
+def death():
+    print("Ты умер")
+
+def panik(inv):
+    if randint(1, 4) == 1:
+        print("Ты находишь ржавый топор, паника помогла!")
+        inv["Топор"] = 1
+        survivers()
+    else:
+        death()
+
+def start():
+    inNum = int(input("""Ты просыпаешься.
 Все что ты знал о современном мире исчезло, люди вокруг обратились в камень.
 Твои действия:
 1. Паниковать
@@ -10,18 +22,18 @@ while loc > -1:
 3. ПАНИКОВАТЬ
 4. Осмотреть местность
 >> """))
-        if inNum in {1, 3}:
-            loc = -2
-            print("Ты паникуешь и падаешь с обрыва")
-            break
-        if inNum == 2:
-            print("Допустим, дам тебе еще один шанс\n")
-        if inNum == 4:
-            if randint(0, 1):
-                print("Ты натыкаешься на группу выживших")
-            else:
-                print("Ты натыкаешься на змею, она тебя кусает и ты погибаешь.")
-                loc = -2
-                break
-if loc == -2:
-    print("Ты умер, игра окончена")
+    if inNum in {1, 2, 3}:
+        panik()
+    else:
+        death()
+    if inNum == 4:
+        survivers()
+def challenge(inv):
+    if randint(0, 1):
+        print("Ты натыкаешься на группу выживших")
+        survivers(inv)
+    else:
+        if inv["Топор"]:
+            print("Ты зарубил змею, будешь ")
+        print("Ты натыкаешься на змею, она тебя кусает и ты погибаешь.")
+    
