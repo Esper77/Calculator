6dc5f602f92603939e2a7df82e09f417389e9add
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-09-24 15:33:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
 from random import randint
 import winsound
 from time import sleep
-inv = {"Топор":0, "Трофей":0}
+import os
+
+
+def clear():
+    os.system('cls')
+
+
+inv = {"Топор": 0, "Трофей": 0}
+
 
 def death():
+    clear()
     sleep(1)
-    if input("Ты умер, начать сначала?") == "да":
+    if input("Ты умер, начать сначала? Да/Нет\n>>").lower() == "да":
         start()
     else:
         exit()
 
+
 def panik(inv):
-    if randint(1, 4) == 1:
-        print("Ты находишь ржавый топор, паника помогла!")
+    clear()
+    if randint(0, 1):
+        print("Ты находишь ржавый топор, паника помогла! Ты начинаешь осматриваться и идешь дальше")
+        sleep(2)
         inv["Топор"] = 1
-        survivers(inv)
+        challenge(inv)
     else:
         print("Ты натыкаешься на змею, она тебя кусает и ты погибаешь.")
+        sleep(3)
         death()
 
+
 def start():
-    inNum = int(input("""Ты просыпаешься.
+    inNum = 1
+    while not (inNum in {"1", "2", "3", "4"}):
+        clear()
+        inNum = (input("""Ты просыпаешься.
 Все что ты знал о современном мире исчезло, люди вокруг обратились в камень.
 Твои действия:
 1. Паниковать
@@ -28,37 +45,63 @@ def start():
 3. ПАНИКОВАТЬ
 4. Осмотреть местность
 >> """))
-    if inNum in {1, 2, 3}:
-        panik(inv)
-    else:
-        sleep(3)
-        challenge(inv)
+        if inNum in {"1", "2", "3"}:
+            panik(inv)
+        elif inNum == "4":
+            sleep(3)
+            challenge(inv)
+        else:
+            print("Неправильно, попробуй еще раз")
+
 
 def challenge(inv):
-    if randint(0, 1):
+    clear()
+    flag = 1
+    if not randint(0, 4):
         print("Ты натыкаешься на группу выживших")
+        sleep(3)
         survivers(inv)
     else:
         if inv["Топор"]:
-            if input("Ты зарубил змею, будешь собирать труп? Да/Нет\n") == "да":
-                print("Ты подобрал труп змеи и пошел дальше")
-                inv["Трофей"] = 1
-            else:
-                print("Ты пошел дальше")
+            sleep(3)
+            while flag:
+                ans = input("Ты зарубил змею, будешь собирать труп? Да/Нет\n").lower()
+                if ans == "да":
+                    print("Ты подобрал труп змеи и пошел дальше")
+                    sleep(3)
+                    inv["Трофей"] = 1
+                    survivers(inv)
+                    flag = 0
+                elif ans == "нет":
+                    print("Ты пошел дальше")
+                    sleep(3)
+                    survivers(inv)
+                    flag = 0
+                else:
+                    print("Неправильно, попробуй еще раз")
         else:
             print("Ты натыкаешься на змею, она тебя кусает и ты погибаешь.")
+            sleep(3)
             death()
 
+
 def survivers(inv):
+    clear()
     if inv["Трофей"]:
-        print("Выжившие встретили тебя как неплохого охотника, ты держал в руках труп ценной в их кругах змеи.")
+        print("Выжившие встретили тебя как неплохого охотника, ты держал в руках труп ценной в этих кругах змеи.")
+        sleep(3)
         win()
     else:
         print("Выжившие тебя не приняли и ты не смог найти себе пропитание в этом враждебном к слабым мире.")
+        sleep(3)
         death()
 
+
 def win():
+    clear()
     print("Ты смог выжить в этом непростом квесте")
     winsound.PlaySound("victory_sJDDywi.wav", winsound.SND_ASYNC)
+    sleep(10)
+
 
 start()
